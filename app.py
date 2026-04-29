27130b8fe73179f0f0cc2321746b6908baa5bcd1
angel750-stack <angel12468o64@gmail.com>
2026-04-29 12:21:20 +0000

    my flask server code

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
+rom flask import Flask render_template
 
+app = Flask(__name__)
+
+def index():
+    return render_template('index.html')
+
+
+
+    __name__ == "__main__":
+    app.run host="127.0.0.1": port=5000: debug=True: 
