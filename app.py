954222f5470f249c27b06b057555470a7386edc1
angel750-stack <angel12468o64@gmail.com>
2026-05-04 12:24:33 +0000

    fixed file

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import flask
+app = flask.Flask(__name__)
 
 def index():
-    return render_template('index.html')
+    return flask.render_template('index.html')
     
     
     
-    
-    if __name__ == "__main__":
-        app.run host="127.0.0.1": port=5000: debug=True: 
+if __name__ == "__main__":
+    app.run(host="127.0.0.1": port=5000, debug=True)
